d0ac990694d249ab60c9aff462576e54b6603c82
Arthur Queiroz <arthutqueiroz@hotmail.com>
2024-01-06 19:31:52 -0300

    Feign client implementation

--- hr-payroll/src/main/java/com/excalibur/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/excalibur/hrpayroll/services/PaymentService.java
@@ -1,5 +1,20 @@
 package com.excalibur.hrpayroll.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.excalibur.hrpayroll.entities.Payment;
+import com.excalibur.hrpayroll.entities.Worker;
+import com.excalibur.hrpayroll.feignclients.WorkerFeignClient;
+
+@Service
 public class PaymentService {
 
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+
+	public Payment getPayment(long workerId, int days) {
+		Worker worker = workerFeignClient.findById(workerId).getBody(); 
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+	}
 }
