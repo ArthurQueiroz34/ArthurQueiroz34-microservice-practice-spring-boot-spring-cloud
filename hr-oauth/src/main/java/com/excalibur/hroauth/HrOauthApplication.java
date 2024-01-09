94d4d746996ad3d825b00d93ba245d3328a6e7f0
Arthur Queiroz <arthutqueiroz@hotmail.com>
2024-01-09 20:31:58 -0300

    OAuth

--- hr-oauth/src/main/java/com/excalibur/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/com/excalibur/hroauth/HrOauthApplication.java
@@ -1,17 +1,15 @@
-package com.excalibur.hrapigatewayzuul;
+package com.excalibur.hroauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableEurekaClient
-@EnableZuulProxy
 @SpringBootApplication
-public class HrApiGatewayZuulApplication {
+public class HrOauthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+		SpringApplication.run(HrOauthApplication.class, args);
 	}
 
 }
